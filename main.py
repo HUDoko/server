e23e69bc56bd90914d3ebf07d999d1defa71f116
HUDoko <oil777oil777@gmail.com>
2022-06-24 18:14:43 +0500

    раскоментил 52 строку в main

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def get_cab_temps_s2(room, start_date):
 
 if __name__ == '__main__':
     threading.Thread(target=lambda: app.run(debug=True, use_reloader=False)).start()
-    #dataloader.period_load()
+    dataloader.period_load()
 
 
 
